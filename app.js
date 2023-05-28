09f1f396792f5f4215d45c7541edf18e5f742bc3
willstorrs <34176205+willstorrs@users.noreply.github.com>
2023-05-28 16:14:09 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,45 +1,18 @@
-
-  // Import the necessary libraries
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-
-// Create a state variable to store the list of companies
-const [companies, setCompanies] = useState([]);
-
-// UseEffect hook to fetch the list of companies from the API
-useEffect(() => {
-  axios.get("https://willstorrs.github.io/companies.json").then((response) => {
-    setCompanies(response.data);
+fetch('companies.json')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error("HTTP error " + response.status);
+    }
+    return response.json();
+  })
+  .then(data => {
+    const dataList = document.getElementById('companies');
+    data.forEach(item => {
+      const option = document.createElement('option');
+      option.value = item.company + " (" + item.ticker + ")";
+      dataList.appendChild(option);
+    });
+  })
+  .catch(function(error) {
+    console.log(error);
   });
-}, []);
-
-// Render the search box and the list of companies
-const App = () => {
-  return (
-    <div>
-      <input
-        type="text"
-        placeholder="Search for a company..."
-        onChange={(e) => {
-          // Filter the list of companies by the search term
-          const filteredCompanies = companies.filter((company) => {
-            return company.name.toLowerCase().includes(e.target.value.toLowerCase());
-          });
-
-          // Set the state variable to the filtered list of companies
-          setCompanies(filteredCompanies);
-        }}
-      />
-      <ul>
-        {companies.map((company) => (
-          <li key={company.id}>
-            {company.name} ({company.ticker})
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-};
-
-export default App;
-  
